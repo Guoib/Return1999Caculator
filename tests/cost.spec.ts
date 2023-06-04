38e329173079ff0937ae3b1e7a8bff6818a2f560
G <18324332230@163.com>
2023-06-04 20:17:01 +0800

    test: added cost test case

---
+++ tests/cost.spec.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+import Back1999Calculator from "../calculator";
+
+describe("cost", () => {
+  const calculator = new Back1999Calculator();
+  Back1999Calculator.energyCostPerHundred = 120;
+
+  it("level30", () => {
+    expect(calculator.calcGiftCost({ 微尘: 300000, 利齿子儿: 120000, 启寤3: 20 }).toFixed(0)).toBe("1665");
+  });
+
+  it("weekly", () => {
+    expect(calculator.calcGiftCost({ 微尘: 100000, 利齿子儿: 60000, 启寤3: 5, 启寤2: 20, 启寤1: 20 }).toFixed(0)).toBe(
+      "958"
+    );
+  });
+});
